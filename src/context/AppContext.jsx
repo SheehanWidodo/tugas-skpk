d4cf00f5d2e4d86376fc43729c6dde6b9c02e544
Sheehan Widodo <sheehanwidodo1996@gmail.com>
2025-03-17 11:19:03 +0700

    Fix AppContext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createContext, useState, useContext } from "react";
 
 const AppContext = createContext()
@@ -38,45 +37,4 @@ function useAppContext() {
     return useContext(AppContext)
 }
 
-=======
-import { createContext, useState, useContext } from "react";
-
-const AppContext = createContext()
-
-function AppProvider(props) {
-    const [dataMaster, setDataMaster] = useState([])
-    const [classes, setClasses] = useState({})
-    const [selectedSubject, setSelectedSubject] = useState({
-        "MAPEL":null,
-        "KODE":null
-    })
-    const [selectedSubjectCode, setSelectedSubjectCode] = useState(null)
-    const [selectedClass, setSelectedClass] = useState({
-        "KELAS":null,
-        "LINK":null
-    })
-
-    return (
-        <AppContext.Provider value={{
-            dataMaster,
-            setDataMaster,
-            classes,
-            setClasses,
-            selectedSubject,
-            setSelectedSubject,
-            selectedSubjectCode,
-            setSelectedSubjectCode,
-            selectedClass,
-            setSelectedClass,
-            }}
-            {...props}
-        />
-    )
-}
-
-function useAppContext() {
-    return useContext(AppContext)
-}
-
->>>>>>> c0b4e8d (Connect VS)
 export { AppProvider, useAppContext }
